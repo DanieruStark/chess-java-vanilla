1421d84de1414b0d7613362173633cde7eb57d24
Daniel Santana <danielmaster19@gmail.com>
2023-07-12 05:16:37 -0300

    validando a posição destino

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,6 +32,8 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
+		
 		Piece capturedPiece = makeMove(source, target);
 
 		return (ChessPiece) capturedPiece;
@@ -52,6 +54,12 @@ public class ChessMatch {
 
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("Não existe movimentos possíveis para a peça escolhida.");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peça selecionada não pode se mover para posição de destino");
 		}
 	}
 
